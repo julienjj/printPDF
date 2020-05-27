58a8cc0dc06fef292586921d6ec416a6c5fbe96c
Julien Victor <53939811+julienjj@users.noreply.github.com>
2020-05-27 09:42:10 -0300

    Add files via upload

---
+++ Imprimir varios PDFs.py
@@ -0,0 +1,16 @@
+import os, re, time
+from tkinter import filedialog
+from tkinter import messagebox
+from tkinter import *
+root = Tk()
+root.withdraw()
+folder_selected = filedialog.askdirectory(initialdir='c://',title="arquivos pdf, impressora padrao")
+
+    
+directory = os.listdir(folder_selected)
+os.chdir(folder_selected)
+
+for file in directory:
+    os.startfile(file, "print")
+
+
